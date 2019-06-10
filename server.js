a792e9da065d7c709368f49a9b118591edad4401
Vishnu Vardhan Karanam <vishnu.karanam@ic0901-x1.corp.inmobi.com>
2019-06-10 15:35:44 +0530

    updating database

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image=require('./Controllers/Image');
 const db=knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : 'postgresql-polished-70460',
       database : 'smartbrain'
     }
   }); 
